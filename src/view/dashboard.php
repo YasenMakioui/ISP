64d6965a73df7115116e27739dff5807b2210fdc
yasin <workspaceyasen@gmail.com>
2022-02-26 00:53:27 +0100

    dependencias

--- src/view/dashboard.php
+++ src/view/dashboard.php
@@ -8,35 +8,29 @@
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item">
-                    <a href="/?vista=dashboard.php&accion=inicio" class="nav-link <?=$active = $_GET['accion'] == 'inicio' ? 'active' : 'text-white'?>" id="homeBtn">
+                    <a href="/?vista=dashboard.php&accion=inicio" class="nav-link" id="homeBtn">
                         Inicio
                     </a>
                 </li>
-                <li>
-                    <a href="/?vista=dashboard.php&accion=resumen" class="nav-link <?=$active = $_GET['accion'] == 'resumen' ? 'active' : 'text-white'?>" id="summaryBtn">
-
-                        Resumen
-                    </a>
-                </li>
-                <li>
-                    <a href="/?vista=dashboard.php&accion=servicios" class="nav-link <?=$active = $_GET['accion'] == 'servicios' ? 'active' : 'text-white'?>">
-
-                        Servicios
-                    </a>
-                </li>
-                <li>
-                    <a href="/?vista=dashboard.php&accion=dns" class="nav-link text-white">
-
-                        dns
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link text-white">
-
-                        apache
-                    </a>
-                </li>
                 <?php //bucle para listar servicios
+                $lista = array("resumen", "serviciosContratados");
+
+                $sql = "SELECT servicio.nombreServicio FROM usuario_servicio INNER JOIN servicio USING(idServicio) ";
+                $result = $conn->query($sql);
+
+                while ($row = $result->fetch_assoc()){
+                    array_push($lista, $row['nombreServicio']);
+                }
+                    
+                foreach ($lista as $value):
+?>
+                <li>
+                    <a href="<?="/?vista=dashboard.php&accion=".$value?>" class="nav-link text-white">
+                        <?=$value?>
+                    </a>
+                </li>
+                <?php
+                endforeach;
                 ?>
             </ul>
             <hr>
@@ -58,38 +52,19 @@
         </div>
     </div>
     <?php
+
     if (isset($_GET['accion'])) {
-        if ($_GET['accion'] == 'resumen') {
-            require_once './src/view/resumen.php';
-        } elseif ($_GET['accion'] == 'dns') {
-            require_once './src/view/dns.php';
-        } elseif ($_GET['accion'] == 'servicios') {
-            require_once './src/view/serviciosContratados.php';
-        } else {
-            echo '<h1>INICIO</h1>';
+        foreach ($lista as $value) {
+            if ($value == $_GET["accion"]) {
+                require_once './src/view/' . $value . '.php';
+            }
         }
     } else {
         echo '<h1>INICIO</h1>';
     }
 
+
+
+
     ?>
 </div>
-
-<script>
-    const home = document.querySelector('#home');
-    const dashboard = document.querySelector('#dashboard');
-    const dns = document.querySelector('#dns');
-
-    const homeBtn = document.querySelector('#homeBtn');
-    const dashboardBtn = document.querySelector('#dashboardBtn');
-
-    homeBtn.addEventListener('click', () => {
-        dashboard.classList.add = 'd-none';
-        dns.classList.add = 'd-none'
-    });
-
-    dashboardBtn.addEventListener('click', () => {
-        home.classList.add('d-none');
-        dns.classList.add('d-none');
-    })
-</script>
--- src/view/serviciosContratados.php
+++ src/view/serviciosContratados.php
@@ -19,13 +19,29 @@
                 <th scope="col">Acción</th>
             </thead>
             <tbody>
+                <?php
+                $sql = "SELECT usuario_servicio.idServicio, servicio.nombreServicio FROM usuario_servicio INNER JOIN servicio USING(idServicio) ";
+                $result = $conn->query($sql);
+                while ($row = $result->fetch_assoc()) :
+                ?>
+                    <tr>
+                        <th scope="row"><?= $row['idServicio'] ?></th>
+                        <td><?= $row['nombreServicio'] ?></td>
+                        <td><?= $row['nombreServicio'] ?></td>
+                        <th scope="col">
+                            <a href="">
+                                <a href="" class="btn btn-danger">BAJA</a>
+                            </a>
+                        </th>
+                    </tr>
+                <?php
+                endwhile;
+                ?>
                 <th scope="row">1</th>
                 <td>Mark</td>
                 <td>Otto</td>
                 <th scope="col">
-                    <a href="">
-                        <i class="bi bi-trash"></i>
-                    </a>
+                    <a href="" class="btn btn-danger">BAJA</a>
                 </th>
             </tbody>
         </table>
